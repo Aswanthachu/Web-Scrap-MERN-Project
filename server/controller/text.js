e99b20a8a3f298aed704182f2d8059bbce7e5222
Aswanthachu <pnkaswanthachu546@gmail.com>
2023-01-28 11:03:05 +0530

    word count api completed

--- server/controller/text.js
+++ server/controller/text.js
@@ -1,4 +1,24 @@
-export const scrapText=async(req,res)=>{
-    console.log("hiii");
-    res.status(200).json({message:"hiii"})
-}
+import puppeteer from "puppeteer";
+
+export const scrapText = async (req, res) => {
+  console.log(req.body);
+  const websiteLink=req.body.web;
+  try {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+
+    await page.goto(websiteLink);
+
+    var text = await page.evaluate(() => document.body.innerText);
+
+    const count = text.split(" ").filter(function (num) {
+      return num != "";
+    }).length;
+    console.log(count);
+
+    await browser.close();
+    res.status(200).json({ count:count });
+  } catch (error) {
+    console.log(error);
+  }
+};
