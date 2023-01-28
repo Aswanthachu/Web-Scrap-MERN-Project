4560d10ce2fe859360aa602660e5c794ca76a0a1
Aswanthachu <pnkaswanthachu546@gmail.com>
2023-01-28 11:09:56 +0530

    text count api completed

--- server/controller/text.js
+++ server/controller/text.js
@@ -2,7 +2,7 @@ import puppeteer from "puppeteer";
 
 export const scrapText = async (req, res) => {
   console.log(req.body);
-  const websiteLink=req.body.web;
+  const websiteLink = req.body.web;
   try {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
@@ -11,13 +11,13 @@ export const scrapText = async (req, res) => {
 
     var text = await page.evaluate(() => document.body.innerText);
 
-    const count = text.split(" ").filter(function (num) {
-      return num != "";
-    }).length;
-    console.log(count);
-
     await browser.close();
-    res.status(200).json({ count:count });
+
+    const count = text.split(" ").filter((txt) => {
+      return txt != "";
+    }).length;
+
+    res.status(200).json({ count: count });
   } catch (error) {
     console.log(error);
   }
