0e21ba46ae461126a9c95a0db3361cc1b36926c2
Aswanthachu <pnkaswanthachu546@gmail.com>
2023-01-29 19:41:19 +0530

    Enviornment variable is created

--- server/index.js
+++ server/index.js
@@ -2,8 +2,11 @@ import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import textRouter from "./router/text.js";
+
+dotenv.config({path:'./config.env'});
 
 const app=express();
 
@@ -12,9 +15,8 @@ app.use(cors());
 
 app.use('/text',textRouter);
 
-const CONNECTION_URL ="mongodb+srv://AswanthAchu:pnk2023@cluster0.ocvutgm.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
+mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
     .then(()=>app.listen(PORT,()=>console.log(`Server Running on ${PORT}`)))
     .catch((err)=>console.log(err));
